1ec33515b38120a2865154fd0e8230da47a29bd7
Lawrence <lawrencegodfrey58@gmail.com>
2021-02-14 10:59:54 +0200

    Added models for posts and comments

--- TellAll/blog/models.py
+++ TellAll/blog/models.py
@@ -1,5 +1,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.BlogPost', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
